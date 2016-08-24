ba9db748e51bff23c3fa0de6b42b2e93e6bdae2b
Usuario <Usuario@DESKTOP-RUQICM2>
2016-08-23 21:35:25 -0500

    añadido visualizar todas las tablas

--- tablasmultiplicar/src/tablasmultiplicar/Tablasmultiplicar.java
+++ tablasmultiplicar/src/tablasmultiplicar/Tablasmultiplicar.java
@@ -20,14 +20,28 @@ public class Tablasmultiplicar {
         // TODO code application logic here
         Scanner lector=new Scanner(System.in);
         System.out.println("Ingenieria de Software2");
-        System.out.println("ingrese la tbla q desee visualizar");
-        
-        int tabla=lector.nextInt();
+        System.out.println("Opciones \n 1)Ingreso de tabla a visualizar \n 2)Visualizar las tablas del 2 al 12");
+        int opcion =lector.nextInt();
         int total;
         
-        for (int i = 1; i <= 12; i++) {
-           total= i*tabla;
-            System.out.println(i+" X "+tabla+" = "+total);
+        if (opcion==1) {
+            System.out.println("ingrese la tbla q desee visualizar");
+            int tabla=lector.nextInt();
+            for (int i = 1; i <= 12; i++) {
+                
+               total= i*tabla;
+                System.out.println(i+" X "+tabla+" = "+total);
+            }
+        }
+        if (opcion==2) {
+                
+            for (int i = 2; i < 12; i++) {
+                for (int j = 1; j < 12; j++) {
+                    total=i*j;
+                    System.out.println(i+" X "+j+" = "+total);
+                }
+            }
+                
         }
     }
     
